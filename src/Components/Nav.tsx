bf4f0ea6e8e2958aa26dd1e3cc20730ac0091d89
wukrit <sukritwalia@gmail.com>
2019-10-25 23:52:17 -0400

    Created basic file structure

---
+++ src/Components/Nav.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "../CSS/Nav.css"
+
+const Nav: React.FC = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Posts</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
